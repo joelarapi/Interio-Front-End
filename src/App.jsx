aaa3cc4d8b16a657eb21bb8edeac22048445b13b
Vjola Sulaj <sulaj.vjolaa@gmail.com>
2024-10-17 19:28:26 +0200

    Business Profile
    Working on business profile

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Components/Login.jsx'
+import ClientProfile from './Components/ClientProfile.jsx';
 
 
 
@@ -12,6 +13,7 @@ function App() {
             <div className="App">
                 <Routes>
                     <Route path="/login" element={<Login />} />
+                    <Route path="/clientprofile" element={<ClientProfile />} />
                 </Routes>
             </div>
         </Router>
