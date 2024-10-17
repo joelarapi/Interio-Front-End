2397a8d21ca3cd073c0b18af5a253ccdbc862baf
joelarapi <arapijoel05@gmail.com>
2024-10-17 16:43:28 +0200

    finished the nav , working on dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Components/Login.jsx'
+import Navbar from './Components/Navbar.jsx';
+import Dashboard from './Components/Dashboard/Dashboard.jsx';
 
 
 
@@ -9,8 +11,10 @@ import Login from './Components/Login.jsx'
 function App() {
     return (
         <Router>
+            <Navbar/>
             <div className="App">
                 <Routes>
+                <Route path="/dashboard" element={<Dashboard/>} />
                     <Route path="/login" element={<Login />} />
                 </Routes>
             </div>
