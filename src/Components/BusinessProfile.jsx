bb4bc24f16454f443f3a26c5d7457f0d48aa1f2c
Vjola Sulaj <sulaj.vjolaa@gmail.com>
2024-10-21 16:14:42 +0200

    Working on Business Page

--- src/Components/BusinessProfile.jsx
+++ src/Components/BusinessProfile.jsx
@@ -27,10 +27,25 @@ const BusinessProfile = () => {
               <Link to={"/"} className={styles.links}>
                 <h5>14 Clients</h5>
               </Link>
+              <div className={styles.profilebutton}>
+                <Link to={"/"} className={styles.links}>
+                  <button>Edit Profile</button>
+                </Link>
+                <Link to={"/"} className={styles.links}>
+                  <button>Promote Now</button>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
         <div className={styles.maininfo}>
+          <div className={styles.aboutbusiness}>
+            <h3>About Business</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim Lorem ipsum dolor sit amet, delit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim Lorem ipsum dolor sit amet
+            </p>
+          </div>
           <div className={styles.innercontainer}>
             <h3>Portfolio</h3>
             <div className={styles.portfoliomedia}>
@@ -38,43 +53,61 @@ const BusinessProfile = () => {
                 <img src="media1.png"></img>
                 <img src="media2.png"></img>
                 <img src="media3.png"></img>
-                <button className={styles.arrows}>&#10095;</button>
               </div>
             </div>
           </div>
-          <div className={styles.innercontainer}>
-            <h3>Reviews</h3>
-            <div className={styles.reviews}>
-              <div className={styles.innerreview}>
-                <img src="Group 4.png"></img>
-                <div className={styles.reviewinfo}>
-                  <h4>Client's Name</h4>
-                  <div className={styles.stars}>
-                    <span>★</span>
-                    <span>★</span>
-                    <span>★</span>
-                    <span>★</span>
-                    <span>★</span>
+          <div className={styles.prevWork}>
+            <h3>Previous Work</h3>
+            <div className={styles.innerwork}>
+              <img src="work.png" alt="work"></img>
+              <div className={styles.work}>
+                <div className={styles.title}>
+                  <h4>Work Title</h4>
+                  <img src="fi-sr-pencil.svg" alt="" />
+                </div>
+                <div className={styles.infos}>
+                  <h5>Date | Location</h5>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minimonsectetur adipiscing elit, incididunt ut labore et dolore magna aliqua. Ut enim ad minim Lorem ipsum dolor sit </p>
+                </div>
+                <hr></hr>
+                <div className={styles.infos}>
+                  <h4 className={styles.title}>Review</h4>
+                  <h5>Client's Name</h5>
+                  <div className={styles.reviewstars}>
+                    <img src="fi-ss-star.png" alt="" />
+                    <img src="fi-ss-star.png" alt="" />
+                    <img src="fi-ss-star.png" alt="" />
+                    <img src="fi-ss-star.png" alt="" />
+                    <img src="fi-ss-star.png" alt="" />
                   </div>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod </p>
-                  <h5>Location | dd/mm/yyyy</h5>
                 </div>
               </div>
             </div>
-            <div className={styles.reviews}>
-              <div className={styles.innerreview}>
-                <img src="Group 4.png"></img>
-                <div className={styles.reviewinfo}>
-                  <h4>Client's Name</h4>
-                  <div className={styles.stars}>
-                    <span>★</span>
-                    <span>★</span>
-                    <span>★</span>
-                    <span>★</span>
-                    <span>★</span>
+            <hr></hr>
+            <div className={styles.innerwork}>
+              <img src="work.png" alt="work"></img>
+              <div className={styles.work}>
+                <div className={styles.title}>
+                  <h4>Work Title</h4>
+                  <img src="fi-sr-pencil.svg" alt="" />
+                </div>
+                <div className={styles.infos}>
+                  <h5>Date | Location</h5>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minimonsectetur adipiscing elit, incididunt ut labore et dolore magna aliqua. Ut enim ad minim Lorem ipsum dolor sit </p>
+                </div>
+                <hr></hr>
+                <div className={styles.infos}>
+                  <h4 className={styles.title}>Review</h4>
+                  <h5>Client's Name</h5>
+                  <div className={styles.reviewstars}>
+                    <img src="fi-ss-star.png" alt="" />
+                    <img src="fi-ss-star.png" alt="" />
+                    <img src="fi-ss-star.png" alt="" />
+                    <img src="fi-ss-star.png" alt="" />
+                    <img src="fi-ss-star.png" alt="" />
                   </div>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod </p>
-                  <h5>Location | dd/mm/yyyy</h5>
                 </div>
               </div>
             </div>
