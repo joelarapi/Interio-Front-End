af2527dfb4b85da38bd96a267a82d415371c3175
Vjola Sulaj <sulaj.vjolaa@gmail.com>
2024-10-24 17:03:27 +0200

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -23,63 +23,16 @@ import BusinessView from "./Components/BusinessView/BusinessView.jsx";
 Amplify.configure(awsExports);
 
 function App() {
-<<<<<<< Updated upstream
-  const location = useLocation();
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-=======
     const location = useLocation();
     const [loading, setLoading] = useState(true);
     useEffect(() => {
         const timer = setTimeout(() => {
             setLoading(false);
         }, 1000);
->>>>>>> Stashed changes
 
     return () => clearTimeout(timer);
   }, []);
 
-<<<<<<< Updated upstream
-  return (
-    <>
-      {location.pathname !== "/login" && location.pathname !== "/landingPage" && <Navbar />}
-      <div className="app">
-        {loading ? (
-          <Loader />
-        ) : (
-<<<<<<< Updated upstream
-          <Routes>
-            <Route path="/landingPage" element={<LandingPage />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/clientprofile" element={<ClientProfile />} />
-            <Route path="/businessprofile" element={<BusinessProfile />} />
-            <Route path="/editprofile" element={<EditProfile />} />
-            <Route path="/businessview" element={<BusinessView />} />
-            <Route path="/post/:id" element={<Post />} />
-          </Routes>
-=======
-          <>
-            <Routes>
-              <Route path="/landingPage" element={<LandingPage />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/clientprofile" element={<ClientProfile />} />
-              <Route path="/businessprofile" element={<BusinessProfile />} />
-              <Route path="/editprofile" element={<EditProfile />} />
-              <Route path="/post/:id" element={<Post />} />
-            </Routes>
-            {/* {location.pathname !== "/login" && location.pathname !== "/landingPage" && <Footer />} */}
-          </>
->>>>>>> Stashed changes
-        )}
-      </div>
-    </>
-  );
-=======
     return (
         <>
             {(location.pathname !== "/login" && location.pathname !== "/landingPage") && <Navbar />}
@@ -102,6 +55,5 @@ function App() {
             </div>
         </>
     );
->>>>>>> Stashed changes
 }
 export default App;
